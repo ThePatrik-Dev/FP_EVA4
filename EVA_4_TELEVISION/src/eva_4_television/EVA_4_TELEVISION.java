418ca845dc6f3f860451f6e87f6734fce2b4cd58
Joel Alderete <thej14653@gmail.com>
2024-08-31 22:06:02 -0600

    Add files via upload

---
+++ EVA_4_TELEVISION/src/eva_4_television/EVA_4_TELEVISION.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_4_television;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Alderete Aguirre Diego Joel
+        
+        String Marca;
+        String Modelo;
+        String Medida;
+        String Tecnologia;
+        int Tamaño;
+        double Precio;
+        
+        Marca = "Samsung";
+        Modelo = "R8";
+        Medida = "Pulgadas";
+        Tecnologia = "OLED";
+        Tamaño = 30;
+        Precio = 3560.99;
+        
+        System.out.println(Marca);
+        System.out.println(Modelo);
+        System.out.println(Medida);
+        System.out.println(Tecnologia);
+        System.out.println(Tamaño);
+        System.out.println(Precio);
+
+        
+    }
+    
+}
